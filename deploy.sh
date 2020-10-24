67cccad02882f45d57e11ea7b6003174d36cc254
Taylor Helsper <tehelsper@gmail.com>
2020-10-23 21:06:33 -0500

    Update settings for deploy to pages

--- deploy.sh
+++ deploy.sh
@@ -4,18 +4,19 @@
 set -e
 
 # build
+rm -rf docs/.vuepress/dist
 npm run docs:build
 
 # navigate into the build output directory
 cd docs/.vuepress/dist
 
 # if you are deploying to a custom domain
-# echo 'www.example.com' > CNAME
+# echo 'www.github.com' > CNAME
 
 git init
 git add -A
 git commit -m 'deploy'
 
-git push -f git@github.com:abhishandy/mynode-docs.git master:gh-pages
+git push -f git@github.com:mynodebtc/mynode_docs.git master:gh-pages
 
 cd -
